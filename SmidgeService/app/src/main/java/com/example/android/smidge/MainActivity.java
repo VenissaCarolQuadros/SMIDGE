541a3874295374cb3af94f3616811b2f63a4a292
VenissaCarolQuadros <venicq@gmail.com>
2023-08-19 12:47:50 -0400

    Revert "User stats included"
    This reverts commit ff5963b8283fd232ed3b93780c8c1ea4ec90eea4.

--- SmidgeService/app/src/main/java/com/example/android/smidge/MainActivity.java
+++ SmidgeService/app/src/main/java/com/example/android/smidge/MainActivity.java
@@ -1,89 +1,146 @@
+
 package com.example.android.smidge;
 
-import static android.app.PendingIntent.getActivity;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import android.app.Activity;
-import android.app.AppOpsManager;
-import android.app.usage.UsageStats;
-import android.app.usage.UsageStatsManager;
-import android.content.ClipData;
-import android.content.ClipboardManager;
+import android.accessibilityservice.AccessibilityService;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.os.Build;
-import android.os.Bundle;
-import android.provider.Settings;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+import android.view.accessibility.AccessibilityEvent;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Map;
-
-public class MainActivity extends AppCompatActivity {
-
-    Long startMillis=System.currentTimeMillis();
-
-    //SharedPreferences preferences = getSharedPreferences(Globals.filename, Context.MODE_PRIVATE);
+public class MainActivity extends AccessibilityService implements SensorEventListener {
+    private Integer twistCount=0, dipCount=0, tiltCount=0;
+    boolean coolDown=false;
+    private Long twistTime=System.nanoTime(), dipTime=System.nanoTime(), tiltTime=System.nanoTime(), coolDownTime=System.nanoTime();
+    Sensor gyroscope;
+    SensorManager sensorManager;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void onAccessibilityEvent(AccessibilityEvent event) {
 
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd G 'at' HH:mm:ss z");
-        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-
-        try {
-            if (!(checkUsageStatsPermission())){
-                Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Globals.timestamp = sdf.format(new Date());
-                Globals.fetchUsageStats(getApplicationContext());
-                String logs=Globals.toJSON(getApplicationContext());
-                ClipData clip = ClipData.newPlainText("Usage Data", logs);
-                clipboard.setPrimaryClip(clip);
-            }
-        });
     }
 
+    @Override
+    public void onInterrupt() {
 
-    private boolean checkUsageStatsPermission() throws PackageManager.NameNotFoundException {
-        PackageManager packageManager = getPackageManager();
-        ApplicationInfo applicationInfo = packageManager.getApplicationInfo(getPackageName(), 0);
-        AppOpsManager appOpsManager = (AppOpsManager) getSystemService(AppCompatActivity.APP_OPS_SERVICE);
-        // `AppOpsManager.checkOpNoThrow` is deprecated from Android Q
-        int mode = 0;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            mode = appOpsManager.unsafeCheckOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS,
-                    applicationInfo.uid, applicationInfo.packageName
-            );
-        }
-        else {
-            appOpsManager.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS,
-                    applicationInfo.uid, applicationInfo.packageName
-            );
-        }
-        return mode == AppOpsManager.MODE_ALLOWED;
     }
-}
+
+    @Override
+    protected void onServiceConnected() {
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        sensorManager.registerListener((SensorEventListener) this, gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event != null && (!coolDown || System.nanoTime() - coolDownTime > 1000000000)) {
+            coolDown=false;
+            String twist1 = "com.google.android.youtube";
+            String twist2 = "com.duolingo";
+            String tilt1 = "com.instagram.android";
+            String tilt2 = "com.microsoft.office.outlook";
+            String dip1 = "com.whatsapp";
+            String dip2 = "com.reddit.frontpage";
+            if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+                // TWIST!
+                if (twistCount == 0) {
+                    twistTime = System.nanoTime();
+                }
+                if (event.values[1] < -5 && twistCount == 1) {
+                    twistCount += 1;
+                    launchApp(twist2);
+                    coolDown=true;
+                    coolDownTime=System.nanoTime();
+                    Log.d("Sensor", "LAUNCHING TWO TWIST INTENT");
+                }
+                if (event.values[1] < -10 && twistCount == 0) {
+                    twistCount += 1;
+                    Log.d("Sensor", "FIRST TWIST $twistCount");
+                    //launchIntent?.let { startActivity(launchIntent) }
+                }
+                if (System.nanoTime() - twistTime > 1000000000) {
+                    twistCount = 0;
+                    launchApp(twist1);
+                    coolDown=true;
+                    coolDownTime=System.nanoTime();
+                    Log.d("Sensor", "LAUNCHING ONE TWIST INTENT");
+                }
+                if (twistCount > 1) {
+                    Log.d("Sensor", "RESETTING TWIST COUNT");
+                    twistCount = 0;
+                }
+
+                // TILT
+                if (tiltCount == 0) {
+                    tiltTime = System.nanoTime();
+                }
+                if (event.values[2] > 5 && tiltCount == 1) {
+                    tiltCount += 1;
+                    launchApp(tilt2);
+                    coolDown=true;
+                    coolDownTime=System.nanoTime();
+                    Log.d("Sensor", "LAUNCHING TWO TILT INTENT");
+                }
+                if (event.values[2] > 10 && tiltCount == 0) {
+                    tiltCount += 1;
+                    Log.d("Sensor", "FIRST TILT $tiltCount");
+                    //launchIntent?.let { startActivity(launchIntent) }
+                }
+                if (System.nanoTime() - tiltTime > 1000000000) {
+                    tiltCount = 0;
+                    launchApp(tilt1);
+                    coolDown=true;
+                    coolDownTime=System.nanoTime();
+                    Log.d("Sensor", "LAUNCHING ONE TILT INTENT");
+                }
+                if (tiltCount > 1) {
+                    Log.d("Sensor", "RESETTING TILT COUNT");
+                    tiltCount = 0;
+                }
+
+                //DIP
+                if (dipCount == 0) {
+                    dipTime = System.nanoTime();
+                }
+                if (event.values[0] > 5 && dipCount == 1) {
+                    dipCount += 1;
+                    launchApp(dip2);
+                    coolDown=true;
+                    coolDownTime=System.nanoTime();
+                    Log.d("Sensor", "LAUNCHING TWO DIP INTENT");
+                }
+                if (event.values[0] > 10 && dipCount == 0) {
+                    dipCount += 1;
+                    Log.d("Sensor", "FIRST DIP $dipCount");
+                    //launchIntent?.let { startActivity(launchIntent) }
+                }
+                if (System.nanoTime() - dipTime > 1000000000) {
+                    dipCount = 0;
+                    launchApp(dip1);
+                    coolDown=true;
+                    coolDownTime=System.nanoTime();
+                    Log.d("Sensor", "LAUNCHING ONE DIP INTENT");
+                }
+                if (dipCount > 1) {
+                    Log.d("Sensor", "RESETTING DIP COUNT");
+                    dipCount = 0;
+                }
+            }}
+    }
+
+            @Override
+            public void onAccuracyChanged (Sensor sensor,int accuracy){
+                return;
+            }
+
+    private void launchApp(String appName){
+        Intent launchIntent= getPackageManager().getLaunchIntentForPackage(appName).setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(launchIntent);
+    }
+
+        }
+
